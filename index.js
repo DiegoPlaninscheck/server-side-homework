0d984a246c942745d251f5bf933975b55975bf16
Gabriel Karing <gabhenkar@gmail.com>
2024-03-31 21:26:14 -0300

    1.0Karingue

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ app.get("/home", (req, res) => {
     res.sendFile(__dirname + "public/home.html");
 });
 
+app.get("/features",(req, res) => {
+    res.sendFile(__dirname + "public/features/features.html");
+})
+
+app.get("/advantages",(req, res) => {
+    res.sendFile(__dirname + "public/advantages/advantages.html");
+})
+
+app.get("/disadvantages",(req, res) => {
+    res.sendFile(__dirname + "public/disadvantages/disadvantages.html");
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
